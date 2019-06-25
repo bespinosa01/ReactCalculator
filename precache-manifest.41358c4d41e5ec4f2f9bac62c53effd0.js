fa8fe920b2015d1d6221c0e4e030277416e3b7d7
Brian Espinosa <bespinosa1931@gmail.com>
2019-06-24 21:07:07 -0500

    Updates

---
+++ precache-manifest.41358c4d41e5ec4f2f9bac62c53effd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d21878519c248b5215b02f553f7dbb",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "2e10a2b64c7d9ff39bf8",
+    "url": "/ReactCalculator/static/css/main.90006847.chunk.css"
+  },
+  {
+    "revision": "64ef0fbed59874139233",
+    "url": "/ReactCalculator/static/js/2.6374e3c7.chunk.js"
+  },
+  {
+    "revision": "2e10a2b64c7d9ff39bf8",
+    "url": "/ReactCalculator/static/js/main.15a19db8.chunk.js"
+  },
+  {
+    "revision": "236c54c985da26a52bc6",
+    "url": "/ReactCalculator/static/js/runtime~main.70542196.js"
+  }
+]);
